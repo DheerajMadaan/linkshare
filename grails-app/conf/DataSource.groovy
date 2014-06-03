4f5ba51a025916cceaf253775d0e17fed3368bd9
DheerajMadaan <dheerajmadaan@intelligrape.com>
2014-06-03 16:24:32 +0530

    changed db name

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -32,7 +32,10 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://localhost:3306/dheeraj_madaan"
+            username = "root"
+            password="igdefault"
+
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
