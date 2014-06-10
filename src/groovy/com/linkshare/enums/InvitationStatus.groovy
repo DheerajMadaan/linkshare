e35d4bae8b1466b4afb446e4f7271311e48e8d55
DheerajMadaan <dheerajmadaan@intelligrape.com>
2014-06-10 09:15:10 +0530

    spelling mistake

--- src/groovy/com/linkshare/enums/InvitationStatus.groovy
+++ src/groovy/com/linkshare/enums/InvitationStatus.groovy
@@ -4,7 +4,7 @@ package com.linkshare.enums
  * Created by dheerajmadaan on 8/6/14.
  */
 public enum InvitationStatus {
-    ACCEPTED('Rccepted'),
+    ACCEPTED('Accepted'),
     REJECTED('Rejected'),
     PENDING('Pending')
     private String invitationStatusCode
